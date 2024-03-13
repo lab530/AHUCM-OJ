bd7a1dc0bb449378eda91fc4bb12c78e6045b3d9
Ivan Chien <qyc027@gmail.com>
2024-03-13 20:10:03 +0800

    fix(docker): add a sudo for Ubuntu host

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,5 +5,9 @@
 pip install -r ./requirements.txt
 python specify.py
 
-cd ../docker && docker-compose up
+if [ -z "$WSL_DISTRO_NAME" ]; then
+    cd ../docker && docker-compose up
+else
+    cd ../docker && sudo docker-compose up
+fi
 
