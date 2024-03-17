15b33536dc7e644f5aa191f769016d6053130397
PlumYu <PlumYu1124@qq.com>
2024-03-17 21:27:25 +0800

    feat: jwt

---
+++ backend/common/jwt.go
@@ -0,0 +1,45 @@
+package common
+
+import (
+	"backend/model"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+// 设置加密密钥
+var jwtKey = []byte("AHUCM_a_secret_crect")
+
+type Claims struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+func ReleaseToken(user model.User) (string, error) {
+	expirationTime := time.Now().Add(3 * time.Hour) // token 有效期三个小时
+	claims := &Claims{
+		UserId: user.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "Admain",
+			Subject:   "user token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ParseToken(tokenString string) (*jwt.Token, *Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	return token, claims, err
+}
